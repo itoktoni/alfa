2fac27ffda92f772056df61bfc3299cb4b62cfea
itoktoni <itok.toni@gmail.com>
2021-12-29 22:11:59 +0700

    updat

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
 
         <div class="login-logo" data-aos="fade-up" data-aos-duration="300">
             <a href="http://accufy.kontrakin.com/">
-                <img class="circles" src="{{ Helper::files('logo/'.config('website.logo')) }}" style="height: 45px;margin-left: 0px;cursor:pointer" alt="{{ config('app.name') }}" />
+                <img class="circles" src="{{ Helper::files('logo/'.env('WEBSITE_LOGO')) }}" style="height: 45px;margin-left: 0px;cursor:pointer" alt="{{ config('app.name') }}" />
             </a>
             <br>
         </div>

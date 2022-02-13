353645300f14a8c8e24d05a3efcd1e259ecce057
itoktoni <itok.toni@gmail.com>
2022-02-13 19:29:57 +0700

    update stock

--- Modules/Report/Resources/views/page/stock/summary_form.blade.php
+++ Modules/Report/Resources/views/page/stock/summary_form.blade.php
@@ -2,30 +2,6 @@
 
 <div class="form-group">
 
-    <label class="col-md-2 col-sm-2 control-label">Dari Tanggal</label>
-    <div class="col-md-4">
-        <div class="input-group">
-            <input type="text" name="from" value="{{ old('from') ?? request()->get('from') ?? date('Y-m-d') }}" class="date">
-            <span class="input-group-addon">
-                <i class="fa fa-calendar"></i>
-            </span>
-        </div>
-    </div>
-
-    <label class="col-md-2 col-sm-2 control-label">Ke Tanggal</label>
-    <div class="col-md-4">
-        <div class="input-group">
-            <input type="text" name="to" value="{{ old('to') ?? request()->get('to') ?? date('Y-m-d') }}" class="date">
-            <span class="input-group-addon">
-                <i class="fa fa-calendar"></i>
-            </span>
-        </div>
-    </div>
-
-</div>
-
-<div class="form-group">
-
     {!! Form::label('name', __('Company'), ['class' => 'col-md-1 col-sm-2 control-label']) !!}
     <div class="col-md-3 col-sm-4 {{ $errors->has('view_company_id') ? 'has-error' : ''}}">
         {{ Form::select('view_company_id', $company, old('view_company_id') ?? request()->get('view_company_id') ?? null, ['class'=> 'form-control ']) }}

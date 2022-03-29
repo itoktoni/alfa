38a7c1f0cfa6662ee91dcd1b4b3234cdfb1bc889
Itok toni <itok.toni@gmail.com>
2022-03-29 22:30:32 +0700

    update parstok

--- Modules/Data/Resources/views/page/stock/excel_report_summary.blade.php
+++ Modules/Data/Resources/views/page/stock/excel_report_summary.blade.php
@@ -4,7 +4,7 @@
             <td width="150">Nama Rumah Sakit</td>
             <td width="100">Nama Ruangan</td>
             <td width="200">Nama Linen</td>
-            <td width="80">Par Stock</td>
+            <td width="80">Register Linen</td>
             <td width="150">Stock Rumah Sakit</td>
             <td width="150">Stock Obsesiman</td>
             <td width="50">Kotor</td>

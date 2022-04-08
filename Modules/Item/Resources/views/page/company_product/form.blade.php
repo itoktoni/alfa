e744ca45ead0e0572b4bad8a3da5cc8cf7e56cdf
Itok toni <itok.toni@gmail.com>
2022-04-08 23:41:43 +0700

    update

--- Modules/Item/Resources/views/page/company_product/form.blade.php
+++ Modules/Item/Resources/views/page/company_product/form.blade.php
@@ -44,15 +44,13 @@
     </div>
     @endif
 
+    <!--
     {!! Form::label('name', __('Location'), ['class' => 'col-md-2 col-sm-2 control-label']) !!}
     <div class="col-md-4 col-sm-4 {{ $errors->has('location_id') ? 'has-error' : ''}}">
         {{ Form::select('location_id', $location, null, ['class'=> 'form-control', ]) }}
         {!! $errors->first('location_id', '<p class="help-block">:message</p>') !!}
     </div>
-
-</div>
-
-<div class="form-group">
+    -->
 
     {!! Form::label('name', __('Product'), ['class' => 'col-md-2 col-sm-2 control-label']) !!}
     @if(isset($master['item_product_id']))
@@ -68,13 +66,21 @@
     </div>
     @endif
 
+</div>
+
+<div class="form-group">
+
     {!! Form::label('name', __('Size'), ['class' => 'col-md-2 col-sm-2 control-label']) !!}
     <div class="col-md-4 col-sm-4 {{ $errors->has('company_item_size_id') ? 'has-error' : ''}}">
         {{ Form::select('company_item_size_id', $size, null, ['class'=> 'form-control', ]) }}
         {!! $errors->first('company_item_size_id', '<p class="help-block">:message</p>') !!}
     </div>
 
-
+    {!! Form::label('name', __('Weight'), ['class' => 'col-md-2 col-sm-2 control-label']) !!}
+    <div class="col-md-4 col-sm-4 {{ $errors->has('company_item_weight') ? 'has-error' : ''}}">
+        {!! Form::text('company_item_weight', null, ['class' => 'form-control']) !!}
+        {!! $errors->first('company_item_weight', '<p class="help-block">:message</p>') !!}
+    </div>
 
 </div>
 
@@ -97,14 +103,8 @@
 
 <div class="form-group">
 
-{!! Form::label('name', __('Weight'), ['class' => 'col-md-2 col-sm-2 control-label']) !!}
-    <div class="col-md-4 col-sm-4 {{ $errors->has('company_item_weight') ? 'has-error' : ''}}">
-        {!! Form::text('company_item_weight', null, ['class' => 'form-control']) !!}
-        {!! $errors->first('company_item_weight', '<p class="help-block">:message</p>') !!}
-    </div>
-
 {!! Form::label('name', __('Description'), ['class' => 'col-md-2 col-sm-2 control-label']) !!}
-    <div class="col-md-4 col-sm-4">
+    <div class="col-md-10 col-sm-10">
         {!! Form::textarea('company_item_description', null, ['class' => 'form-control', 'rows' => '3']) !!}
     </div>
 <!--

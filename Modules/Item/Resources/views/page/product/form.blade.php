e001a8878db87b201f69d0743d38ce4c4622e28b
Itoktoni <itok@kidsoft.com.au>
2022-09-01 22:42:46 +0700

    remove active

--- Modules/Item/Resources/views/page/product/form.blade.php
+++ Modules/Item/Resources/views/page/product/form.blade.php
@@ -16,21 +16,11 @@
 
 <div class="form-group">
 
-    {!! Form::label('name', __('Active'), ['class' => 'col-md-2 col-sm-2 control-label']) !!}
-    <div class="col-md-4 col-sm-4 {{ $errors->has('item_product_status') ? 'has-error' : ''}}">
-        {{ Form::select('item_product_status', ['0' => 'No', '1' => 'Yes'], null, ['class'=> 'form-control ']) }}
-        {!! $errors->first('item_product_status', '<p class="help-block">:message</p>') !!}
-    </div>
-
     {!! Form::label('name', __('Category'), ['class' => 'col-md-2 col-sm-2 control-label']) !!}
     <div class="col-md-4 col-sm-4 {{ $errors->has('item_product_category_id') ? 'has-error' : ''}}">
         {{ Form::select('item_product_category_id', $category, null, ['class'=> 'form-control ']) }}
         {!! $errors->first('item_product_category_id', '<p class="help-block">:message</p>') !!}
     </div>
-
-</div>
-
-<div class="form-group">
 
     {!! Form::label('name', __('Logo'), ['class' => 'col-md-2 col-sm-2 control-label']) !!}
     <div class="col-md-4 col-sm-4" {{ $errors->has('item_product_image') ? 'has-error' : ''}}">
@@ -40,8 +30,12 @@
         {!! $errors->first('item_product_image', '<p class="help-block">:message</p>') !!}
     </div>
 
+</div>
+
+<div class="form-group">
+
     {!! Form::label('name', __('Description'), ['class' => 'col-md-2 col-sm-2 control-label']) !!}
-    <div class="col-md-4 col-sm-4">
+    <div class="col-md-10 col-sm-10">
         {!! Form::textarea('item_product_description', null, ['class' => 'form-control', 'rows' => '3']) !!}
     </div>
 

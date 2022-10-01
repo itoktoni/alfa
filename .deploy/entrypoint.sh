b188043ca1be76a8187b74b35a7387a88652ba43
Itoktoni <itok@kidsoft.com.au>
2022-10-01 22:59:28 +0700

    update

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -2,12 +2,12 @@
 
 echo "🎬 entrypoint.sh: [$(whoami)] [PHP $(php -r 'echo phpversion();')]"
 
-composer dump-autoload --no-interaction --no-dev --optimize
+#composer dump-autoload --no-interaction --no-dev --optimize
 
 echo "🎬 artisan commands"
 
 # 💡 Group into a custom command e.g. php artisan app:on-deploy
-php artisan migrate --no-interaction --force
+#php artisan migrate --no-interaction --force
 
 echo "🎬 start supervisord"
 

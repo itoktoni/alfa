beba5ba33771794985108eea697552310a1fa3c3
Itoktoni <itok@kidsoft.com.au>
2022-12-27 00:45:32 +0700

    update

--- app/Http/Middleware/checkAction.php
+++ app/Http/Middleware/checkAction.php
@@ -17,7 +17,8 @@ class checkAction {
     {
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
 
         return $next($request);
 
